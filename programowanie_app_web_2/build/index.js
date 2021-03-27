519e0f680d45f98ef199ef91fd80f8451c1bd484
Dominik Kozioł <dkoziol097@gmail.com>
2021-03-27 16:29:30 +0100

    style progress

--- programowanie_app_web_2/build/index.js
+++ programowanie_app_web_2/build/index.js
@@ -5,38 +5,60 @@ var Drumkit = /** @class */ (function () {
     Drumkit.prototype.appStart = function () {
         var _this = this;
         this.setSounds();
+        this.setDivs();
         this.setChanels();
         document.addEventListener("keydown", function (event) { _this.onKeyDown(event); });
         var chanel1 = document.querySelector("#chanel-1");
         chanel1.addEventListener("click", function () { _this.playChanel1(); });
     };
+    Drumkit.prototype.recordChanel1 = function () {
+    };
     Drumkit.prototype.onKeyDown = function (event) {
-        console.log(event);
         var key = event.key;
+        // var timeFromBegin = event.timeStamp;
         var time = event.timeStamp;
         this.chanel1.push({ key: key, time: time });
         this.playSound(key);
     };
     Drumkit.prototype.playSound = function (key) {
+        var div = this.divDictionary[key];
+        div.style.backgroundColor = "#44fb38";
         var audio = this.soundsDictionary[key];
         audio.currentTime = 0;
         audio.play();
+        setTimeout(function () { return div.style.backgroundColor = "#efefef"; }, 300);
     };
     Drumkit.prototype.setSounds = function () {
+        var matches = document.querySelectorAll("audio[data-song]");
+        console.log(matches);
         this.soundsDictionary = {
-            "q": new Audio('./assets/sounds/hihat.wav'),
-            "w": new Audio('./assets/sounds/boom.wav'),
-            "e": new Audio('./assets/sounds/clap.wav'),
-            "r": new Audio('./assets/sounds/kick.wav'),
-            "t": new Audio('./assets/sounds/openhat.wav'),
-            "y": new Audio('./assets/sounds/ride.wav'),
-            "u": new Audio('./assets/sounds/snare.wav'),
-            "i": new Audio('./assets/sounds/tink.wav'),
-            "o": new Audio('./assets/sounds/tom.wav')
+            "q": matches[0],
+            "w": matches[1],
+            "e": matches[2],
+            "r": matches[3],
+            "t": matches[4],
+            "y": matches[5],
+            "u": matches[6],
+            "i": matches[7],
+            "o": matches[8]
+        };
+    };
+    Drumkit.prototype.setDivs = function () {
+        this.divDictionary = {
+            "q": document.querySelector("#q"),
+            "w": document.querySelector("#w"),
+            "e": document.querySelector("#e"),
+            "r": document.querySelector("#r"),
+            "t": document.querySelector("#t"),
+            "y": document.querySelector("#y"),
+            "u": document.querySelector("#u"),
+            "i": document.querySelector("#i"),
+            "o": document.querySelector("#o")
         };
     };
     Drumkit.prototype.playChanel1 = function () {
         var _this = this;
+        console.log(this.chanel1);
         this.chanel1.forEach(function (element) {
             setTimeout(function () { return _this.playSound(element.key); }, element.time);
         });
--- programowanie_app_web_2/src/index.ts
+++ programowanie_app_web_2/src/index.ts
@@ -4,48 +4,77 @@ class Drumkit {
     chanel3: Chanel[];
     chanel4: Chanel[];
     soundsDictionary: { [key: string]: HTMLAudioElement };
+    divDictionary: { [char: string]: HTMLElement };
     constructor() {
         this.appStart();
     }
 
     appStart(): void {
         this.setSounds();
+        this.setDivs();
         this.setChanels();
         document.addEventListener("keydown", (event: KeyboardEvent) => { this.onKeyDown(event) });
         const chanel1 = document.querySelector("#chanel-1");
-        chanel1.addEventListener("click", () => { this.playChanel1()} );
-        
+        chanel1.addEventListener("click", () => { this.playChanel1() });
+
+    }
+
+    public recordChanel1() {
+
     }
 
     onKeyDown(event: KeyboardEvent): void {
-        console.log(event);
         var key = event.key;
+        // var timeFromBegin = event.timeStamp;
         var time = event.timeStamp;
         this.chanel1.push({ key, time });
         this.playSound(key);
     }
 
     playSound(key: string): void {
+       
+        var div = this.divDictionary[key];
+        div.style.backgroundColor = "#44fb38";
         var audio = this.soundsDictionary[key];
         audio.currentTime = 0
         audio.play();
+        setTimeout(() => div.style.backgroundColor = "#efefef", 300)
+        
     }
 
     setSounds(): void {
+        var matches: NodeListOf<HTMLAudioElement> = document.querySelectorAll("audio[data-song]");
+        console.log(matches);
         this.soundsDictionary = {
-            "q": new Audio('./assets/sounds/hihat.wav'),
-            "w": new Audio('./assets/sounds/boom.wav'),
-            "e": new Audio('./assets/sounds/clap.wav'),
-            "r": new Audio('./assets/sounds/kick.wav'),
-            "t": new Audio('./assets/sounds/openhat.wav'),
-            "y": new Audio('./assets/sounds/ride.wav'),
-            "u": new Audio('./assets/sounds/snare.wav'),
-            "i": new Audio('./assets/sounds/tink.wav'),
-            "o": new Audio('./assets/sounds/tom.wav')
+            "q": matches[0],
+            "w": matches[1],
+            "e": matches[2],
+            "r": matches[3],
+            "t": matches[4],
+            "y": matches[5],
+            "u": matches[6],
+            "i": matches[7],
+            "o": matches[8],
+
+        }
+    }
+    setDivs(): void {
+        this.divDictionary = {
+            "q": document.querySelector("#q"),
+            "w": document.querySelector("#w"),
+            "e": document.querySelector("#e"),
+            "r": document.querySelector("#r"),
+            "t": document.querySelector("#t"),
+            "y": document.querySelector("#y"),
+            "u": document.querySelector("#u"),
+            "i": document.querySelector("#i"),
+            "o": document.querySelector("#o"),
+
         }
     }
 
     playChanel1(): void {
+        console.log(this.chanel1)
         this.chanel1.forEach(element => {
             setTimeout(() => this.playSound(element.key), element.time)
         });
@@ -62,6 +91,7 @@ class Drumkit {
 class Chanel {
     key: string;
     time: number;
+    //timeFromBegin: number;
 }
 
 
